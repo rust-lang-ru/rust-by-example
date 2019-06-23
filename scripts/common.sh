9eb5ff7b1e7ec2a18e01a7977242697aae57b64f
funkill2 <funkill2@gmail.com>
2019-06-23 13:33:24 +0300

    added update original script

---
+++ scripts/common.sh
@@ -0,0 +1,9 @@
+SCRIPTS_DIR=`dirname "$0" | xargs realpath`
+ROOT_DIR=`realpath "${SCRIPTS_DIR}/../"`
+ORIGINAL_DIR="${ROOT_DIR}/rust-by-example"
+ORIGINAL_LANG="en"
+
+TRANSLATION_LANG="ru"
+TRANSLATION_DIR="${ORIGINAL_DIR}-${TRANSLATION_LANG}"
+
+#
---
+++ scripts/update_original.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euv
+
+CURRENT_DIR=`dirname $0`
+source "${CURRENT_DIR}/common.sh"
+
+ORIGINAL_REPO="https://github.com/rust-lang/rust-by-example.git"
+
+cd "${ROOT_DIR}"
+
+git pull origin master
+rm -rf "${ORIGINAL_DIR}"
+git clone --depth=1 "${ORIGINAL_REPO}" "${ORIGINAL_DIR}"
+rm -rf "${ORIGINAL_DIR}/.git"
+
+git add -A "${ORIGINAL_DIR}"
+git commit -m 'update original'
+#git push origin master
+
+#
